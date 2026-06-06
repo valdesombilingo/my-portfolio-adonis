f9df475dd1d94bddcf1f419f1851d07826246e59
Valdes OMBILINGO <smilogod@gmail.com>
2026-06-06 20:55:57 +0100

    feat: setup pages controller, routes, views, and main layout structure

--- start/routes.ts
+++ start/routes.ts
@@ -7,24 +7,13 @@
 |
 */
 
-import { middleware } from '#start/kernel'
-import { controllers } from '#generated/controllers'
+const PagesController = () => import('#controllers/pages_controller')
+
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home').as('home')
-
-router
-  .group(() => {
-    router.get('signup', [controllers.NewAccount, 'create'])
-    router.post('signup', [controllers.NewAccount, 'store'])
-
-    router.get('login', [controllers.Session, 'create'])
-    router.post('login', [controllers.Session, 'store'])
-  })
-  .use(middleware.guest())
-
-router
-  .group(() => {
-    router.post('logout', [controllers.Session, 'destroy'])
-  })
-  .use(middleware.auth())
+router.group(() => {
+  router.on('/').render('pages/home').as('home')
+  router.get('/experience', [PagesController, 'experience']).as('experience')
+  router.get('/projects', [PagesController, 'projects']).as('projects')
+  router.get('/contact', [PagesController, 'contact']).as('contact')
+})
