49abb2a296ce56f1287d499686ca7644a241c6fc
Valdes OMBILINGO <smilogod@gmail.com>
2026-09-09 18:14:22 +0100

    fix(portfolio): remove obsolete auth code

--- .adonisjs/server/controllers.ts
+++ .adonisjs/server/controllers.ts
@@ -7,5 +7,4 @@ export const controllers = {
   Contact: () => import('#controllers/contact_controller'),
   NewAccount: () => import('#controllers/new_account_controller'),
   Pages: () => import('#controllers/pages_controller'),
-  Session: () => import('#controllers/session_controller'),
 }
